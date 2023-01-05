0c96ac42ad11e9e8f4c5959904cc2dd3abf767a8
brunaumLL <bruno99louzada@gmail.com>
2023-01-05 16:27:06 -0300

    req 2 feito

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,5 +1,5 @@
 import csv
-from statistics import multimode
+from statistics import mode
 
 
 def analyze_log(path_to_file):
@@ -10,11 +10,11 @@ def analyze_log(path_to_file):
             file_inf = csv.reader(file)
             data = [item for item in file_inf]
             list = organizer_list(data)
-            mrm = most_requsted_maria(list)
-            ttar = times_that_arnaldo_requested(list)
-            jno = joao_never_order(list)
-            jngtsb = joao_never_go_to_snack_bar(list)
-            save_analyze(mrm, ttar, jno, jngtsb)
+            mo = most_orded(list, "maria")
+            to = times_orded(list, "arnaldo", "hamburguer")
+            no = never_orded(list, "joao")
+            dnv = days_never_visited(list, "joao")
+            save_analyze(mo, to, no, dnv)
     except FileNotFoundError:
         raise FileNotFoundError(f'Arquivo inexistente. {path_to_file}')
 
@@ -31,56 +31,71 @@ def organizer_list(data):
     return list
 
 
-def most_requsted_maria(list):
-    maria_order = []
+def most_orded(list, client):
+    order = []
     for obj in list:
-        if obj['client'] == 'maria':
-            maria_order.append(obj['order'])
-    most_requested = (multimode(maria_order))
-    return most_requested[0]
+        if obj['client'] == client:
+            order.append(obj['order'])
+    most_requested = (mode(order))
+    return most_requested
 
 
-def times_that_arnaldo_requested(list):
-    arnaldo_request = []
+def times_orded(list, client, order):
+    orded = []
     for obj in list:
-        if obj['client'] == 'arnaldo':
-            arnaldo_request.append(obj['order'])
-    times_requested = arnaldo_request.count('hamburguer')
-    return times_requested
+        if obj['client'] == client:
+            orded.append(obj['order'])
+    times_orded = orded.count(order)
+    return times_orded
 
 
-def joao_never_order(list):
-    joao_orders = []
+def never_orded(list, client):
     orders = []
+    all_orders = []
     never_order = []
     for obj in list:
-        orders.append(obj['order'])
-        if obj['client'] == 'joao':
-            joao_orders.append(obj['order'])
+        all_orders.append(obj['order'])
+        if obj['client'] == client:
+            orders.append(obj['order'])
 
-    for order in orders:
-        if order not in joao_orders:
+    for order in all_orders:
+        if order not in orders:
             never_order.append(order)
     return set(never_order)
 
 
-def joao_never_go_to_snack_bar(list):
-    joao_go = []
+def days_never_visited(list, client):
+    days_visited = []
     days = []
-    never_go = []
+    never_visited = []
     for obj in list:
         days.append(obj['day'])
-        if obj['client'] == 'joao':
-            joao_go.append(obj['day'])
+        if obj['client'] == client:
+            days_visited.append(obj['day'])
 
     for day in days:
-        if day not in joao_go:
-            never_go.append(day)
-    return set(never_go)
+        if day not in days_visited:
+            never_visited.append(day)
+    return set(never_visited)
 
 
-def save_analyze(mrm, ttar, jno, jngtsb):
+def busiest_day(list):
+    days_visited = []
+    for obj in list:
+        days_visited.append(obj['day'])
+    most_visited = (mode(days_visited))
+    return most_visited
+
+
+def least_busy_day(list):
+    days_visited = []
+    for obj in list:
+        days_visited.append(obj['day'])
+    lst = days_visited[::-1]
+    return min(lst, key=lst.count)
+
+
+def save_analyze(mo, to, no, dnv):
     with open('data/mkt_campaign.txt', 'w') as file:
-        analyze = (f'{mrm}\n{ttar}\n{jno}\n{jngtsb}')
-        print(analyze)
+        analyze = (f'{mo}\n{to}\n{no}\n{dnv}')
         file.write(analyze)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,38 @@
+from src.analyze_log import organizer_list, most_orded, busiest_day
+from src.analyze_log import never_orded, days_never_visited, least_busy_day
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self.orders = []
+
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append([customer, order, day])
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        list = organizer_list(self.orders)
+        most_ordered = most_orded(list, customer)
+        return most_ordered
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        list = organizer_list(self.orders)
+        never_ordered = never_orded(list, customer)
+        return never_ordered
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        list = organizer_list(self.orders)
+        never_visited = days_never_visited(list, customer)
+        return never_visited
 
     def get_busiest_day(self):
-        pass
+        list = organizer_list(self.orders)
+        most_visited = busiest_day(list)
+        return most_visited
 
     def get_least_busy_day(self):
-        pass
+        list = organizer_list(self.orders)
+        least_visited = least_busy_day(list)
+        return least_visited
